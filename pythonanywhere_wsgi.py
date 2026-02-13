96aa143511b443546f405ae6db38f6971ce0a5d5
hmddotcom <hmddotcom@gmail.com>
2026-02-13 22:48:23 +0200

    Fix PythonAnywhere WSGI configuration - correct paths and sys.path for hotel_project module

--- pythonanywhere_wsgi.py
+++ pythonanywhere_wsgi.py
@@ -7,9 +7,14 @@ import os
 import sys
 
 # Add your project directory to the sys.path
-path = '/home/yourusername/Hotelerie'
+path = '/home/netsama/HotelMgr'
 if path not in sys.path:
     sys.path.append(path)
+
+# Also add the parent directory for proper module resolution
+parent_path = '/home/netsama'
+if parent_path not in sys.path:
+    sys.path.append(parent_path)
 
 # Set environment variables
 os.environ['DJANGO_SETTINGS_MODULE'] = 'hotel_project.settings_production'
